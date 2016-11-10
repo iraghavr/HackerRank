ca29177e363d34fe503ce5de8e4eeaf6a97a8081
Adrian Statescu <mergesortv@gmail.com>
2016-11-10 09:26:43 -0800

    circulararray rotation solution accepted

---
+++ CircularArrayRotation/CircularArrayRotation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// rotating the array.
+int main() {
+	// Read input
+	int n, k, q;
+	cin>>n>>k>>q;
+	int *array = new int[n];
+	for(int i = 0; i < n; i++) {
+		cin>>array[i];
+	}
+	k %= n; // Remove the number of full array rotations from k
+	for(int i = 0; i < q; i++) {
+		int b;
+		cin>>b;
+		cout<<array[(n-k+b)%n]<<"\n"; // Calculate the new index for b
+	}
+    return 0;
+}
